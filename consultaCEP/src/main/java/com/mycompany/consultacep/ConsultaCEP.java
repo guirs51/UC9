5f5657f03fe8ca0654a33cd1c3731d0e2aa97ec9
Guilherme Dias Villanova <143645273+guirs51@users.noreply.github.com>
2025-05-13 16:14:34 -0300

    Add files via upload

---
+++ consultaCEP/src/main/java/com/mycompany/consultacep/ConsultaCEP.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.consultacep;
+
+import com.mycompany.consultacep.view.CadastroForm;
+
+/**
+ *
+ * @author GUILHERMEDIASVILLANO
+ */
+public class ConsultaCEP {
+
+    public static void main(String[] args) {
+       new CadastroForm().setVisible(true);
+    }
+}
